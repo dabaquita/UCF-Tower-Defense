32a85e41e5c92fd457a6131708a8c4022872d6bb
Recleph Mere <lilrayzer1@gmail.com>
2021-02-06 08:46:40 -0500

    Add a sample project that demonstrates a demo 'Welcome' screen file structure for the project

---
+++ SampleProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        //SceneManager.LoadScene("FIRST_SCENE_OF_THE_GAME");
+    }
+
+}
