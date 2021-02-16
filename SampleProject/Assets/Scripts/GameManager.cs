05034c5adf4cf05791a8f92b14899d5d33bf5406
matthews242 <matthews242@gmail.com>
2021-02-15 20:38:23 -0500

    Added Player class and GameManager class

---
+++ SampleProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.Health <= 0)
+        {
+            Debug.Log("Lives is 0");
+            SceneManager.LoadScene(sceneName:"MenuScene");
+        }
+
+    }
+}
---
+++ SampleProject/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static int Health;
+    public int startingHealth = 100;
+
+    public static int Money;
+    public int startingMoney = 300;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = startingHealth;
+        Money = startingMoney;
+    }
+}
