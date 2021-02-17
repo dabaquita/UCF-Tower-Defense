6cdd15419908ac0e2e72904276a2b4989356e16d
Nicholas Nachowicz <nicholasnachowicz@knights.ucf.edu>
2021-02-17 18:46:25 -0500

    grid and grid test

---
+++ SampleProject/Assets/Scripts/Testing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    Grid grid;
+    int value;
+    private void Start() {
+        grid = new Grid(82, 42, 1f, new Vector3(-41,-21));
+    }
+ 
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), 10);
+        }
+ 
+        if (Input.GetMouseButtonDown(1)) {
+            Debug.Log(grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        }
+    }
+}
