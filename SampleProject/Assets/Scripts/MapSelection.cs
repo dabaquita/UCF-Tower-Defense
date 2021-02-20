a8b52ea55dda79dee50e05293cc2b1a3042d0048
matthews242 <matthews242@gmail.com>
2021-02-20 16:08:50 -0500

    added a map selection and attached script to switch maps

---
+++ SampleProject/Assets/Scripts/MapSelection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MapSelection : MonoBehaviour
+{
+    public void ToMemoryMall()
+    {
+        SceneManager.LoadScene(sceneName: "MemoryMallScene");
+    }
+
+    public void ToGreekRow()
+    {
+        SceneManager.LoadScene(sceneName: "GreekRowScene");
+    }
+
+    public void ToStudentUnion()
+    {
+        SceneManager.LoadScene(sceneName: "StudentUnionScene");
+    }
+
+    public void ToBounceHouse()
+    {
+        Debug.Log("BOUNCE BOUNCE");
+        SceneManager.LoadScene(sceneName: "BounceHouseScene");
+    }
+
+    public void ToCB1()
+    {
+        SceneManager.LoadScene(sceneName: "CB1ClassroomScene");
+    }
+
+    public void ToLibrary()
+    {
+        SceneManager.LoadScene(sceneName: "LibraryScene");
+    }
+}
