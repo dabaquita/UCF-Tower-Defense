61c2053ad0b60b377e9c4025a287155bdde14adc
matthews242 <matthews242@gmail.com>
2021-02-27 01:24:35 -0500

    enemies now die when they're killed (or reach the end)

--- SampleProject/Assets/Enemy2.cs
+++ SampleProject/Assets/Enemy2.cs
@@ -1,33 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PathCreation;
 
-public class Enemy : MonoBehaviour
+public class Enemy2 : MonoBehaviour
 {
     public int Health;
     public static int startingHealth = 50;
 
-    // public pathCreator pathCreator;
-    // public float speed = 5;
-    // float distanceTravelled;
-
-
-    // for testing
-    public static Vector3 endOfMap = new Vector3(1,2,3);
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTravelled;
+    public EndOfPathInstruction endOfPathInstruction;
 
     // Start is called before the first frame update
     void Start()
     {
-        Health = Enemy.startingHealth;
-        transform.position = Enemy.endOfMap;
+        Health = Enemy2.startingHealth;
+        transform.position = pathCreator.path.GetPointAtTime(0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        // distanceTravelled += speed * Time.deltaTime;
-        // transform.position = pathCreator.path.getPointAtDistance(distanceTravelled);
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, EndOfPathInstruction.Stop);
 
 
         if (IsDead())
@@ -38,16 +36,18 @@ public class Enemy : MonoBehaviour
         // will be true when the enemy hits the end of the map while still having health
         // using 3 since the array in Path.cs only has 4 segments and I assume that's the last
         // one
-        if (transform.position.Equals(endOfMap))
+
+        if (IsAtEnd())
         {
             DamagePlayer();
+            DestroyEnemy();
         }
     }
 
     public void DamagePlayer()
     {
         Player.Health -= 10;
-        // DestroyEnemy();
+        DestroyEnemy();
     }
 
     public bool IsDead()
@@ -62,5 +62,14 @@ public class Enemy : MonoBehaviour
     public void DestroyEnemy()
     {
         Destroy(gameObject);
+    }
+
+    public bool IsAtEnd()
+    {
+        if (transform.position == pathCreator.path.GetPoint(730))
+        {
+            return true;
+        }
+        return false;
     }
 }
