275f72693780990363283b42d4b57aee72cd1fac
matthews242 <matthews242@gmail.com>
2021-03-04 02:51:33 -0500

    Created audio mixer and script to control audio. Added to sliders.

---
+++ SampleProject/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetBGMVolume (float sliderValue)
+    {
+        mixer.SetFloat("MusicExposed", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetSFXVolume(float sliderValue)
+    {
+        mixer.SetFloat("SFXExposed", Mathf.Log10(sliderValue) * 20);
+    }
+}
