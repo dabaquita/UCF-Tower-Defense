e00579c53f82ea2bfe25413518843b2e740a4e66
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-03-26 20:38:15 -0400

    Started survival spawner

--- SampleProject/Assets/Scripts/WaveSpawning/SurvivalSpawner.cs
+++ SampleProject/Assets/Scripts/WaveSpawning/SurvivalSpawner.cs
@@ -4,9 +4,40 @@ using UnityEngine;
 public class SurvivalSpawner : WaveSpawner
 {
     private int waveNumber;
+    private int waveDifficulty;
+    private string[] enemyNames;
+
+    /*
+     * int Wave::enemiesPerWave()
+{
+    int rsp = (int)((0.15 * this->pWaveNumber) * (24 + 6 * (this->pWaveDifficulty-1)));
+    return rsp;
+}
+     */
 
     public SurvivalSpawner(GameObject[] enemies) : base(enemies)
     {
         waveNumber = 0;
+        waveDifficulty = 0;
+
+        enemyNames = new string[enemies.Length];
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemyNames[i] = enemies[i].name;
+        }
+    }
+
+    private List<GameObject> GetNextWave()
+    {
+        List<GameObject> wave = new List<GameObject>();
+
+
+
+        return wave;
+    }
+
+    private int GetNumEnemiesPerwave()
+    {
+        return (int)(0.15 * (waveNumber + 1)) * (20 * (waveDifficulty + 1));
     }
 }
